fe97e657c9727836d2be82f6f998a242e986843a
HannaYershova <hanna.yershova@ucu.edu.ua>
2022-05-03 23:31:05 +0300

    add homework

---
+++ homework.js
@@ -0,0 +1,139 @@
+/* DONT CHANGE THIS CODE - START */
+function wait(ms = 1000) { 
+    return new Promise(resolve => setTimeout(resolve, ms)) 
+}
+
+class Dish {
+    constructor(cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    async cook() {
+        const actualCookingTime = this.cookingTime * (1 + Math.random()) * 100;
+        await wait(actualCookingTime);
+        return this;
+    }
+}
+/* DONT CHANGE THIS CODE - END */
+
+/* YOUR CODE HERE */
+
+class Ingridient {
+    constructor(name, amount) {
+      this.name = name;
+      this.amount = amount;
+    }
+}
+
+class Bolognese extends Dish {
+    constructor() {
+      super(10);
+      this.ingridients = {
+        meat: 1,
+        spaghetti: 1,
+        tomato: 1,
+      };
+    }
+}
+  
+class MashedPotatoes extends Dish {
+    constructor() {
+      super(8);
+      this.ingridients = {
+        potato: 1,
+      };
+    }
+}
+  
+class Steak extends Dish {
+    constructor() {
+      super(7);
+      this.ingridients = {
+        meat: 2,
+      };
+    }
+}
+
+class SteakAndFries extends Dish {
+    constructor() {
+      super(15);
+      this.ingridients = {
+        meat: 2,
+        potato: 1,
+      };
+    }
+}
+
+class Kitchen {
+    constructor() {
+        this.fridge = {};
+        this.orders = new Array();
+    }
+
+    addToFridge(ingridients) {
+        for (const i of ingridients) {
+            if (i.name in this.fridge) {
+                this.fridge[i.name] += i.amount;
+            } else {
+                this.fridge[i.name] = i.amount;  
+            }
+        }
+    }
+    
+    order(dish) {
+        for (const [name, amount] of Object.entries(dish.ingridients)) {
+          if (name in this.fridge) {
+            if (this.fridge[name] - amount >= 0) {
+              this.fridge[name] -= amount;
+            } else {
+              throw "Not enough ingridients in fridge";
+            }
+          }
+        }
+        this.orders.push(dish);
+    }
+    
+    async cookFastestOrder() {
+        let fastestDish = this.orders[0];
+        for (const dish of this.orders) {
+            if (dish.cookingTime < fastestDish.cookingTime) {
+                fastestDish = dish;
+            }
+        }
+        this.orders = this.orders.filter((e) => e !== fastestDish);
+        await fastestDish.cook();
+        return fastestDish;
+    }
+    
+    async cookAllOrders() {
+        for (const order of this.orders) {
+          await order.cook();
+        }
+        let allOrders = this.orders;
+        this.orders = new Array();
+        return allOrders;
+    }
+}
+
+async function test() {
+    const kitchen = new Kitchen();
+    kitchen.addToFridge([
+        new Ingridient('potato', 1),
+        new Ingridient('spaghetti', 1),
+        new Ingridient('meat', 3),
+        new Ingridient('tomato', 2)
+    ])
+
+    kitchen.order(new Bolognese()); // Bolognese extends Dish (cookingTime = 10)
+    kitchen.order(new MashedPotatoes()); // MashedPotatoes extends Dish (cookingTime = 8)
+    kitchen.order(new Steak()); // Steak extends Dish (cookingTime = 7)
+
+    // Feel free to experiment with various dishes and ingridients
+
+    await kitchen.cookFastestOrder(); // Returns fastest dish to make
+    await kitchen.cookAllOrders(); // Returns two dishes in array
+
+    kitchen.order(new SteakAndFries()); // Throws Error: Not enough ingridients in fridge
+}
+
+test();
